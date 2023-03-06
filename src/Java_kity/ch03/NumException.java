82471d14b95288b8f9ea879ef4e8e55c96c99afa
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-03-06 15:00:28 +0900

    3장 예제
    배열

---
+++ src/Java_kity/ch03/NumException.java
@@ -0,0 +1,18 @@
+package Java_kity.ch03;
+
+public class NumException {
+    public static void main(String[] args) {
+        String[] stringNumber = {"23", "12", "3.141592", "998"};
+
+        int i=0;
+        try {
+            for (i = 0; i < stringNumber.length; i++) {
+                int j = Integer.parseInt(stringNumber[i]); // 3.141592 를 정수로 변환할 때 NumberFormatException 예외 발생
+                System.out.println("정수로 변환된 값은 " + j);
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println(stringNumber[i] + "는 정수로 변환할 수 없습니다.");
+        }
+    }
+}
