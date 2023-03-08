61b2630e57cb4df8303b16533fbb2416dbc90ade
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-03-08 12:35:34 +0900

    4장 예제
    생성자 생성

---
+++ src/Java_kity/ch04/Book.java
@@ -0,0 +1,22 @@
+package Java_kity.ch04;
+
+public class Book {
+    String title;
+    String author;
+
+    public Book(String t) { // 생성자
+        title = t;
+        author = "작자 미상";
+    }
+    public Book(String t, String a) { // 생성자
+        title = t;
+        author = a;
+    }
+
+    public static void main(String[] args) {
+        Book littlePrince = new Book("어린왕자", "생텍쥐페리"); // 생성자 Book (String t, String a) 호출
+        Book loveStroy = new Book("춘향전"); // 생성자 Book(String t) 호출
+        System.out.println(littlePrince.title + " " + littlePrince.author);
+        System.out.println(loveStroy.title + " " + loveStroy.author);
+    }
+}
--- src/Java_kity/ch04/Circle.java
+++ src/Java_kity/ch04/Circle.java
@@ -3,7 +3,13 @@ package Java_kity.ch04;
 public class Circle {
     int radius; // 원의 반지름
     String name; // 원의 이름 필드
-    public Circle() {} //생성자
+    public Circle() { //매개 변수 없는 생성자
+        radius = 1; name =""; //필드 초기화
+
+    }
+    public Circle(int r, String n) {// 매개 변수를 가진 생성자
+        radius = r; name = n; //매개 변수로 필드 초기화
+    }
 
     public double getArea() { // 원의 면적 계산 메소드
         return 3.14*radius*radius;
@@ -11,15 +17,14 @@ public class Circle {
 
     public static void main(String[] args) {
         Circle pizza;
-        pizza = new Circle();
-        pizza.radius = 10;
-        pizza.name = "자바 피자";
+        pizza = new Circle(10, "자바 피자"); //객체 생성, 반지름 10으로, 이름을 "자바피자'로 초기화
+
         double area = pizza.getArea();
         System.out.println(pizza.name + "의 면적은 " + area);
 
-        Circle donut = new Circle();
-        donut.radius = 2;
-        donut.name = "자바 도넛";
+        Circle donut = new Circle(); //객체 생성. 반지름을 1, 이름을 ""로 초기화
+
+        donut.name = "도넛 피자"; // 이름 변경
         area = donut.getArea();
         System.out.println(donut.name + "의 면적은 " + area);
     }
