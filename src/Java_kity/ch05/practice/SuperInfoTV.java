d9b17d5adb9c59410eb6d890e79f8d53c4f17432
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-03-15 20:49:35 +0900

    5장 실습문제
    서브 클래스 만들기

--- src/Java_kity/ch05/practice/SuperInfoTV.java
+++ src/Java_kity/ch05/practice/SuperInfoTV.java
@@ -19,13 +19,29 @@ class ColorTV extends TV {
     public void printProperty() {
         System.out.println(getSize()+"인치 "+color+"컬러");
     }
+    protected int getColor() {
+        return color;
+    }
 
+}
+class IPTV extends ColorTV {
+    private String IP;
+    public IPTV(String IP, int size, int color) {
+        super(size, color);
+        this.IP = IP;
+    }
+    public void printProperty() {
+        System.out.println(IP+" 주소의 "+getSize()+"인치 "+getColor()+"컬러");
+    }
 }
 
 public class SuperInfoTV {
     public static void main(String[] args) {
         ColorTV myTV = new ColorTV(32, 1024);
         myTV.printProperty();
+        IPTV iptv = new IPTV("192.1.1.2", 32, 2048);
+        System.out.print("나의 IPTV는 ");
+        iptv.printProperty();
 
     }
 }
