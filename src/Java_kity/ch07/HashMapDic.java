9ea6144b017038591b97e0ed9310e686deafbef6
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-03-31 11:36:57 +0900

    7장 예제
    HashMap

---
+++ src/Java_kity/ch07/HashMapDic.java
@@ -0,0 +1,25 @@
+package Java_kity.ch07;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class HashMapDic {
+    public static void main(String[] args) {
+        HashMap<String, String> dic = new HashMap<String, String>();
+        dic.put("baby", "아기");
+        dic.put("love", "사랑");
+        dic.put("apple", "사과");
+
+        Scanner s = new Scanner(System.in);
+        while(true) {
+            System.out.print("영단어 입력>> ");
+            String word = s.next();
+            if(word.equals("exit"))
+                break;
+            String kor = dic.get(word);
+            System.out.println(word + ":" + kor);
+        }
+        System.out.println("종료합니다......");
+    }
+}
