3d436248b4f587002f61f0ba551a3cd82b16439a
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-04-05 09:02:32 +0900

    자바

---
+++ src/Java_kity/ch07/practice/Solution.java
@@ -0,0 +1,17 @@
+package Java_kity.ch07.practice;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList();
+        System.out.println();
+    }
+
+
+}
