440f24b6364e23707dd8f9988e82a573f913f1b0
heejinkong <113762366+heejinkong@users.noreply.github.com>
2024-03-20 19:03:29 +0900

    3
    3333

---
+++ src/loop/loopEx/ForEx3.java
@@ -0,0 +1,13 @@
+package loop.loopEx;
+
+public class ForEx3 {
+    public static void main(String[] args) {
+        int max = 2;
+        int sum = 0;
+
+        for(int i = 1; i <= max; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
