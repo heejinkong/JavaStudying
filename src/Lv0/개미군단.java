77f66d9266b2c597695695f4a054fd453745e552
heejinkong <113762366+heejinkong@users.noreply.github.com>
2024-04-29 18:00:25 +0900

    day 6,7

---
+++ src/Lv0/개미군단.java
@@ -0,0 +1,21 @@
+package Lv0;
+
+public class 개미군단 {
+    public static void main(String[] args) {
+        int hp = 23;
+        System.out.println(solution(hp));
+    }
+
+    private static int solution(int hp) {
+        int answer = 0;
+        answer += (hp / 5);
+        hp %= 5;
+
+        answer += (hp / 3);
+        hp %= 3;
+
+        answer += (hp / 1);
+
+        return answer;
+    }
+}
---
+++ src/Lv0/양꼬치.java
@@ -0,0 +1,19 @@
+package Lv0;
+
+public class 양꼬치 {
+    public static void main(String[] args) {
+        int n = 10;
+        int k = 3;
+        System.out.println(solution(n,k));
+    }
+
+    private static int solution(int n, int k) {
+        int n_money, k_money = 0;
+        n_money = n * 12000;
+        int service = n / 10;
+        k_money = (k-service) * 2000;
+
+        int answer = n_money + k_money;
+        return answer;
+    }
+}
