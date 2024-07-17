735b9fb5de450f1657fde5897af293ebbbc641d1
heejinKong <gmlwls3379@naver.com>
2024-07-17 23:32:16 +0900

    코테

---
+++ src/Lv0/대문자와소문자.java
@@ -0,0 +1,23 @@
+package Lv0;
+
+import java.util.Arrays;
+
+public class 대문자와소문자 {
+    public static void main(String[] args) {
+        String my_string = "cccCCC";
+        System.out.println(solution(my_string));
+    }
+    public static String solution(String my_string) {
+        String answer = "";
+
+        for(int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if(Character.isUpperCase(c)){
+                answer += Character.toLowerCase(c);
+            }else{
+                answer += Character.toUpperCase(c);
+            }
+        }
+        return answer;
+    }
+}
