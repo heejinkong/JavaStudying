bf6a710f8e0b886e7066280262b5f6fa8944b208
heejinKong <gmlwls3379@naver.com>
2024-07-29 16:25:57 +0900

    pgs_lv0

---
+++ src/Lv0/세균증식.java
@@ -0,0 +1,14 @@
+package Lv0;
+
+public class 세균증식 {
+    class Solution {
+        public int solution(int n, int t) {
+            int answer = n;
+            for(int i=1; i <= t; i++) {
+                answer *= 2;
+                System.out.println(answer);
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/Lv0/자릿수더하기.java
@@ -0,0 +1,15 @@
+package Lv0;
+
+public class 자릿수더하기 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            String s = Integer.toString(n);
+
+            for(int i=0; i<s.length(); i++){
+                answer += Integer.parseInt(s.substring(i, i+1));
+            }
+            return answer;
+        }
+    }
+}
