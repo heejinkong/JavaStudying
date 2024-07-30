52865d8ffb8f6de24ccbd2386fbc8c4984453bcd
heejinKong <gmlwls3379@naver.com>
2024-07-30 21:50:35 +0900

    pgs_lv0

---
+++ src/Lv0/소인수분해.java
@@ -0,0 +1,28 @@
+package Lv0;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class 소인수분해 {
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(solution(n));
+    }
+    public static int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 2; i <=n; i++) {
+            if(n % i == 0) {
+                while(n % i ==0) {
+                    n /= i;
+                }
+                list.add(i);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
