a51319f0bb28d20eea9cc0578e8477f81036ecae
heejinKong <gmlwls3379@naver.com>
2024-11-01 12:10:24 +0900

    D2_1926런타임에러

---
+++ src/SW/D2/D2.java
@@ -0,0 +1,18 @@
+package SW.D2;
+import java.util.Scanner;
+public class D2 {
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+
+
+        }
+    }
+}
