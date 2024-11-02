bfaf8a0c0f2cb3b6bac6f903ae6cf37db17a69f3
heejinKong <gmlwls3379@naver.com>
2024-11-02 13:59:03 +0900

    D2_1926

--- src/SW/D2/D2_1926.java
+++ src/SW/D2/D2_1926.java
@@ -1,34 +1,26 @@
 package SW.D2;
 import java.util.Scanner;
-
 public class D2_1926 {
+    public static int n;
     public static void main(String args[]) throws Exception {
+
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+        int n = sc.nextInt();
+        sc.close();
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            String tmp = String.valueOf(i);
 
-            // 각 테스트 케이스마다 한 줄에 출력
-            for (int i = 1; i <= n; i++) {
-                String tmp = String.valueOf(i);
-
-                if (tmp.contains("3") || tmp.contains("6") || tmp.contains("9")) {
-                    // "3", "6", "9"이 포함된 경우 "-"를 출력
-                    for (int j = 0; j < tmp.length(); j++) {
-                        if (tmp.charAt(j) == '3' || tmp.charAt(j) == '6' || tmp.charAt(j) == '9') {
-                            System.out.print("-");
-                        }
+            if (tmp.contains("3") || tmp.contains("6") || tmp.contains("9")) {
+                for (int j = 0; j < tmp.length(); j++) {
+                    if (tmp.charAt(j) == '3' || tmp.charAt(j) == '6' || tmp.charAt(j) == '9') {
+                        System.out.print("-");
                     }
-                    System.out.print(" ");
-                } else {
-                    // 포함되지 않은 경우 숫자 출력
-                    System.out.print(i + " ");
                 }
+            } else {
+                System.out.print(i);
             }
-            System.out.println(); // 각 테스트 케이스마다 줄바꿈
+            System.out.print(" ");
         }
-
-        sc.close(); // Scanner 닫기
     }
 }
