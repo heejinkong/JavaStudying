eda9657412ef1fdabf44b26fb270df526ffc0bb5
heejinKong <gmlwls3379@naver.com>
2024-11-03 15:47:01 +0900

    D2_1974

---
+++ src/SW/D2/D2_1974.java
@@ -0,0 +1,63 @@
+package SW.D2;
+import java.util.Scanner;
+
+public class D2_1974 {
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        int[][] map = new int[9][9];
+
+
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int answer = 1;
+            for(int i = 0; i < 9; i++) {
+                for(int j = 0; j < 9; j++) {
+                    map[i][j] = sc.nextInt();
+                }
+            }
+
+            for(int i = 0; i < 9; i++) {
+                int  rowSum = 0;
+                int colSum = 0;
+                for(int j = 0; j < 9; j++) {
+                    rowSum += map[i][j];
+                    colSum += map[j][i];
+                }
+
+                if(rowSum != 45 || colSum != 45) {
+                    answer = 0;
+                    break;
+                }
+            }
+
+            if(answer == 0) {
+                System.out.println("#" + test_case + " " + answer);
+                continue;
+            }
+
+            for(int i = 1; i <= 3; i++) {
+                int squareSum = 0;
+                for(int j = (i - 1) * 3; j < i * 3; j++) {
+                    for(int k = (i - 1) * 3; k < i * 3; k++) {
+                        squareSum += map[j][k];
+                    }
+                }
+
+                if(squareSum != 45) {
+                    answer = 0;
+                    break;
+                }
+                if(answer == 0) {
+                    break;
+                }
+            }
+            System.out.println("#" + test_case + " " + answer);
+
+        }
+    }
+}
