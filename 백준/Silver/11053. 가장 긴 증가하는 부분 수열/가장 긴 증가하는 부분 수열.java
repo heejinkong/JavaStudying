d81663322cffcb328b8a05a0eec5b9208e18ceef
heejin Kong <113762366+heejinkong@users.noreply.github.com>
2024-12-10 23:23:04 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 292 ms, Memory: 20924 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1; 
+        
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+
+        }
+
+        System.out.println(max);
+
+    }
+}
