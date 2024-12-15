d98b40670fdcb25978d3b735d7880cd9df0a0239
heejin Kong <113762366+heejinkong@users.noreply.github.com>
2024-12-15 17:01:00 +0900

    [Bronze II] Title: 벌집, Time: 180 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int cnt = 1;
+        int range =1;
+        
+        while (N > range) { 
+            range += 6 * cnt; 
+            cnt++; 
+        }
+
+        System.out.println(cnt);
+        
+        
+    }
+}
